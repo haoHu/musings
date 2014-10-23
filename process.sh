c86ccea8844916cfcced126e544eef1926125be8
okay <okay@chert>
2014-10-22 21:32:08 -0700

    move files to inc/ dir

--- process.sh
+++ process.sh
@@ -10,25 +10,29 @@ fi
 OUT_DIR="${DIR}/musincs"
 mkdir -p ${OUT_DIR}
 
+DATA_FILE="${OUT_DIR}/data.js"
+
 # prepare the data for JSON formatting
 
-echo "(function() { var JSON_DATA = [ " > data.js
+echo "(function() { var JSON_DATA = [ " > ${DATA_FILE}
 for file in `cat ${OUT_DIR}/analyzed.log`; do
   music_name=`basename $file`
-  echo "{ name: '${music_name}'," >> data.js
-  echo "file: '${file}'," >> data.js
+  echo "{ name: '${music_name}'," >> "${DATA_FILE}"
+  echo "file: '${file}'," >> "${DATA_FILE}"
 
   bpm=`cat ${OUT_DIR}/${music_name}.bpm`
-  echo "bpm: ${bpm}," >> data.js
-  echo "chords: [ " >> data.js
-  chords=`cat ${OUT_DIR}/${music_name}.chords`
-  echo "$chords" | sed -e 's/^/[/' -e 's/$/],/' >> data.js
+  echo "bpm: ${bpm}," >> ${DATA_FILE}
+  echo "chords: [ " >> ${DATA_FILE}
+  chords=`cat "${OUT_DIR}/${music_name}.chords"`
+  echo "$chords" | sed -e 's/^/[/' -e 's/$/],/' >> ${DATA_FILE}
 
-  echo "]}, " >> data.js
+  echo "]}, " >> "${DATA_FILE}"
 done
 
-echo -e "];" >> data.js
+echo -e "];" >> "${DATA_FILE}"
 
-echo "load_data(JSON_DATA);" >> data.js
-echo -e "})();\n" >> data.js
+echo "load_data(JSON_DATA);" >> ${DATA_FILE}
+echo -e "})();\n" >> ${DATA_FILE}
 
+cp viewer.html $OUT_DIR/index.html
+cp -R inc/ $OUT_DIR
