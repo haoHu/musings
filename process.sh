1a1fa3872c854a487d1d8aab769711f7569d1333
okay <okay@chert>
2014-10-25 11:32:38 -0700

    updating viewer, removing process.py

--- process.sh
+++ process.sh
@@ -12,26 +12,29 @@ mkdir -p ${OUT_DIR}
 
 DATA_FILE="${OUT_DIR}/data.js"
 
+PROJECT_NAME=`dirname "${DIR}"`
+
 # prepare the data for JSON formatting
 
 echo "(function() { var JSON_DATA = [ " > ${DATA_FILE}
-for file in `cat ${OUT_DIR}/analyzed.log`; do
-  music_name=`basename $file`
+while read line; do
+  file=$line
+  music_name=`basename "$file"`
   echo "{ name: '${music_name}'," >> "${DATA_FILE}"
   echo "file: '${file}'," >> "${DATA_FILE}"
 
-  bpm=`cat ${OUT_DIR}/${music_name}.bpm`
-  echo "bpm: ${bpm}," >> ${DATA_FILE}
-  echo "chords: [ " >> ${DATA_FILE}
+  bpm=`cat "${OUT_DIR}/${music_name}.bpm"`
+  echo "bpm: ${bpm}," >> "${DATA_FILE}"
+  echo "chords: [ " >> "${DATA_FILE}"
   chords=`cat "${OUT_DIR}/${music_name}.chords"`
   echo "$chords" | sed -e 's/^/[/' -e 's/$/],/' >> ${DATA_FILE}
 
   echo "]}, " >> "${DATA_FILE}"
-done
+done < ${OUT_DIR}/analyzed.log
 
 echo -e "];" >> "${DATA_FILE}"
 
-echo "load_data(JSON_DATA);" >> ${DATA_FILE}
+echo "load_data(JSON_DATA, '${PROJECT_NAME}');" >> ${DATA_FILE}
 echo -e "})();\n" >> ${DATA_FILE}
 
 cp viewer.html $OUT_DIR/index.html
